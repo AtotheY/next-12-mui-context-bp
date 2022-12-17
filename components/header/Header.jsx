9482d2df5d1f78db1698863cdea051a0e493e801
Anthony Sistilli <sistillianthony@gmail.com>
2022-12-16 22:05:19 -0500

    header pos

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
   const s = styles(theme);
 
   return (
-    <AppBar sx={{ height: '60px' }}>
+    <AppBar position="relative" sx={{ height: '60px' }}>
       <Toolbar sx={s.toolbar}>
         <Typography>bee.ly</Typography>
         <Box sx={{ px: 4, py: 3 }}>
